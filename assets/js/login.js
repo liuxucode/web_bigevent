2028b644853c964f0655dead692464c2a74c8f6c
LXTech <xliu.ce.scu@gmail.com>
2023-02-15 00:29:50 +0000

    完成文章管理功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -59,7 +59,7 @@ $(function() {
                     //将登录成功得到的token字符串，保存到localStorage中
                 localStorage.setItem('token', res.token)
                     //跳转到后台主页
-                location.href = 'http://localhost:8080/day1test/index.html'
+                location.href = '/day1test/index.html'
 
             }
 
